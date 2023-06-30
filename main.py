9219f113a07118a9cf9fdb4ecf196bacea40dfc4
tod619 <trevorodwyer619@hotmail.com>
2023-06-30 21:02:25 +0100

    Add label in main.py file

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ window = tkinter.Tk()
 window.title("My first tkinter GUI program")
 window.minsize(width=500, height=500)
 
+# Create a label
+my_label = tkinter.Label(text="I am a new label", font=("Arial", 24, "bold"))
+my_label.pack()
+
 window.mainloop()
