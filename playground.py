8983f3d5670745e8e6716771530f300a7158917f
tod619 <trevorodwyer619@hotmail.com>
2023-06-30 21:52:54 +0100

    Create calculate function that takes multiple keyword args in playground.py file

--- playground.py
+++ playground.py
@@ -1,4 +1,4 @@
-# Experiment with functions that take multiple arguments
+# Experiment with functions that take multiple positional arguments
 def add(*args):
     total = 0
     for num in args:
@@ -9,3 +9,16 @@ def add(*args):
 
 sum = add(5, 6, 10, 20, 5)
 print(sum)
+
+# experiment with function that take multiple key word argurments
+
+
+def calculate(n, **kwargs):
+
+    n += kwargs["add"]
+    n *= kwargs["multiple"]
+
+    return n
+
+
+print(calculate(7, add=10, multiple=15))
