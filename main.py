96341be28ae8410e25ea89bbe87afd1b5cc27cfe
tod619 <trevorodwyer619@hotmail.com>
2023-07-02 21:14:13 +0100

    Add multi line comment box in main.py file

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first tkinter GUI program")
 window.minsize(width=500, height=500)
 
 # Create a label
-my_label = tkinter.Label(text="I am a new label", font=("Arial", 24, "bold"))
+my_label = Label(text="I am a new label", font=("Arial", 24, "bold"))
 my_label.pack()
 
 # Create a function for the btm
@@ -18,11 +18,21 @@ def button_clicked():
 
 
 # Create a btn
-button = tkinter.Button(text="Click Me", command=button_clicked)
+button = Button(text="Click Me", command=button_clicked)
 button.pack()
 
 # Create an entry compontent (input)
-input = tkinter.Entry(width=10)
+input = Entry(width=10)
 input.pack()
+
+# Text
+text = Text(height=5, width=30)
+# Puts cursor in textbox.
+text.focus()
+# Adds some text to begin with.
+text.insert(END, "Example of multi-line text entry.")
+# Get's current value in textbox at line 1, character 0
+print(text.get("1.0", END))
+text.pack()
 
 window.mainloop()
