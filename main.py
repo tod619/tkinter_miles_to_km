dec5c21c843e74fd27260adc940baf10d3a6989d
tod619 <trevorodwyer619@hotmail.com>
2023-07-02 21:20:37 +0100

    Create radio btn in main.py file

--- main.py
+++ main.py
@@ -60,4 +60,19 @@ checkbutton = Checkbutton(
 checked_state.get()
 checkbutton.pack()
 
+
+# Radiobutton
+def radio_used():
+    print(radio_state.get())
+
+
+# Variable to hold on to which radio button value is checked.
+radio_state = IntVar()
+radiobutton1 = Radiobutton(text="Option1", value=1,
+                           variable=radio_state, command=radio_used)
+radiobutton2 = Radiobutton(text="Option2", value=2,
+                           variable=radio_state, command=radio_used)
+radiobutton1.pack()
+radiobutton2.pack()
+
 window.mainloop()
