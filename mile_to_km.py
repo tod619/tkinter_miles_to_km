042df355bf500f75340d8966c819339543d1117a
tod619 <trevorodwyer619@hotmail.com>
2023-07-03 22:05:01 +0100

    create + display window object in mile_to_km.py file

--- mile_to_km.py
+++ mile_to_km.py
@@ -2,3 +2,11 @@
 # this program converts miles to kilometer through a GUI
 # 03/07/2023
 from tkinter import *
+
+# create window
+window = Tk()
+window.title("Miles to Kilometer Converter")
+
+
+# Display window until closed
+window.mainloop()
