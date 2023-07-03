9146350de1093c986a4c4fad2a2df432877bd979
tod619 <trevorodwyer619@hotmail.com>
2023-07-03 22:19:04 +0100

    create + position widgets in mile_to_km.py file

--- mile_to_km.py
+++ mile_to_km.py
@@ -7,6 +7,26 @@ from tkinter import *
 window = Tk()
 window.title("Miles to Kilometer Converter")
 
+# Create widgets
+miles_input = Entry()
+miles_input.grid(column=1, row=0)
+
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
+
+is_equal_label = Label(text="is equal to")
+is_equal_label.grid(column=0, row=1)
+
+kilometer_result_label = Label(text="0")
+kilometer_result_label.grid(column=1, row=1)
+
+
+kilometer_label = Label(text="Km")
+kilometer_label.grid(column=2, row=1)
+
+calculate_btn = Button(text="Caluclate")
+calculate_btn.grid(column=1, row=2)
+
 
 # Display window until closed
 window.mainloop()
